9d1fc46ad06af6c39a2d40b8c61665029030a165
haoyan <haoyan.uiuc@gmail.comscript type=text/javascript(function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(script);l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); /Applications /Developer /Library /Network /System /User Information /Users /Volumes /bin /cores /dev /etc /home /mach_kernel /net /opt /private /sbin /tmp /usr /var ]] FinalReport/ FinalResult/ MidReport/ Proposal/ photoshare/ /script>
2013-12-13 22:19:10 -0600

    rknn attempt

--- rknn.R
+++ rknn.R
@@ -1,5 +1,6 @@
 # random knn
 library('rknn')
+library('FNN')
 load("/Users/haoyan/Dropbox/STAT542_Shared/testFeature2_modified.RData")
 load("/Users/haoyan/Dropbox/STAT542_Shared/trainFeature2_modified.RData")
 
@@ -13,4 +14,12 @@ sample_index <- sample(1:n_train,round(0.8*n_train))
 train_rknn <- trainFeature[sample_index,]
 test_rknn <- trainFeature[-sample_index,]
 
+train.Y <- trainFeature[,1]
+train.X <- trainFeature[,2:9]
+
+test.X<- testFeature[,2:9]
+train.Y <- factor(train.Y)
+model<-knn.cv(train.X,train.Y, k = 1, prob = TRUE)
+knn(train.X, test.X, train.Y, k = 1, prob=TRUE)
+attributes(.Last.value)
 
